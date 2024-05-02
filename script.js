3e0a76fa0e8154b0abd930ebba50cf637d38fda7
JeromeChalletAspace <cjerome@premiumartsinc.com>
2024-05-02 16:54:36 +0900

    Refactoring For Project Architecture

--- script.js
+++ script.js
@@ -11,79 +11,100 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+//////////////REFACTORING FOR PROJECT ARCHITECTURE//////////////
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    // we need to use the bind keyword to bind this keyword to the current object
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('could not get your position');
+        }
+      );
+    }
+  }
+
+  // in a regular function call , the this keyword is set as undefined
+  _loadMap(position) {
+    console.log('position: ', position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log('lat long: ', latitude, longitude);
+    console.log(
+      `https://www.google.com/maps/@${latitude},${longitude},14z?entry=ttu`
+    );
+
+    const coords = [latitude, longitude];
+
+    // L is the namespace of Leaflet so its uses leaflet apii on usch and such variable
+    // 2nd value is zoom level
+    this.#map = L.map('map').setView(coords, 13);
+    // now that map is an object of the leaflet library, we can see its methods
+    console.log(this.#map);
+
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    //////////////DISPLAYING A MAP MARKER//////////////
+    //////////////RENDERING WORKOUT INPUT FORM//////////////
+
+    // handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    // display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('workout')
+      .openPopup();
+  }
+}
+
+const app = new App();
 
 //////////////USING THE GEOLOCATION API//////////////
 // takes in 2 callback function, 1st for success, 2nd for error
 // the st callback function has a position parameter
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      console.log('position: ', position);
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log('lat long: ', latitude, longitude);
-      console.log(
-        `https://www.google.com/maps/@${latitude},${longitude},14z?entry=ttu`
-      );
-
-      const coords = [latitude, longitude];
-
-      // L is the namespace of Leaflet so its uses leaflet apii on usch and such variable
-      // 2nd value is zoom level
-      map = L.map('map').setView(coords, 13);
-      // now that map is an object of the leaflet library, we can see its methods
-      console.log(map);
-
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      //////////////DISPLAYING A MAP MARKER//////////////
-      //////////////RENDERING WORKOUT INPUT FORM//////////////
-      // handling clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert('could not get your position');
-    }
-  );
-}
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-
-  // display marker
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('workout')
-    .openPopup();
-});
-
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
